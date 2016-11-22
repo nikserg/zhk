f48b813bde24b1573e5fcaff964f16180d7cf20c
Nikita Zarubin <n.zarubin@itsirius.ru>
2016-11-22 16:55:32 +0300

    master

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'yii2basic',
+    'password' => 'yii2basic',
     'charset' => 'utf8',
 ];
